01bd52cae7b432822d6e6cf194ea526d59f4d5cc
geovana borges <141837580+geovanaborgex@users.noreply.github.com>
2026-06-17 15:01:31 -0300

    alterei para conseguir buscar horarios no render

--- google_calendar.php
+++ google_calendar.php
@@ -4,11 +4,35 @@ require __DIR__ . '/vendor/autoload.php';
 
 date_default_timezone_set('America/Sao_Paulo');
 
-putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/extensao-luiza-2469f7a587b1.json');
+/*
+|--------------------------------------------------------------------------
+| Credenciais Google no Render
+|--------------------------------------------------------------------------
+*/
+
+$credentialsJson = getenv('GOOGLE_CREDENTIALS');
+
+if (!$credentialsJson) {
+    die('Variável GOOGLE_CREDENTIALS não configurada no Render.');
+}
+
+$tempCredentialsFile = __DIR__ . '/google_credentials.json';
+
+file_put_contents($tempCredentialsFile, $credentialsJson);
+
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $tempCredentialsFile);
+
+/*
+|--------------------------------------------------------------------------
+| Google Calendar
+|--------------------------------------------------------------------------
+*/
 
 $client = new Google_Client();
 $client->useApplicationDefaultCredentials();
-$client->setScopes(Google_Service_Calendar::CALENDAR);
+$client->setScopes([
+    Google_Service_Calendar::CALENDAR
+]);
 
 $service = new Google_Service_Calendar($client);
 
