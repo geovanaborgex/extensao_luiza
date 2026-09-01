cf70810b2bd31d1cc9371065cc0f9ed18c0b0ffb
geovanaborgex <geovanaborges304@gmail.com>
2026-09-01 12:23:21 -0300

    arrumando responsividade para telas menores das pages admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,17 +1,10 @@
 <?php
 
 header('Content-Type: application/json; charset=utf-8');
-
 require '../google_calendar.php';
-
 date_default_timezone_set('America/Sao_Paulo');
-
-
 $calendarId = 'luizagues99@gmail.com';
-
 $acao = $_GET['acao'] ?? '';
-
-
 
 /*
 ====================================================
