938d19d63ce9ff52c1a3b53b88f6a5524cb03b2f
geovanaborgex <geovanaborges304@gmail.com>
2026-09-24 13:39:36 -0300

    arrumando menu lateral das telas admin

--- admin/js/relatorios.js
+++ admin/js/relatorios.js
@@ -140,7 +140,7 @@ async function buscarAgendamentos(inicio = "", fim = ""){
 
     try{
 
-        let url = "dashboard.php?acao=listar";
+        let url = "/api/dashboard.php?acao=listar";
 
         if(inicio !== "" && fim !== ""){
 
